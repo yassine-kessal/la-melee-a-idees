4bafa1de5a9048e6c5f72590b88f6ed7ccf9342c
Yassine Kessal <yassinekessalmain@gmail.com>
2020-01-26 14:19:11 +0100

    Add new observable variable and update authenticate function to get user data from database

--- src/stores/user-store.js
+++ src/stores/user-store.js
@@ -1,4 +1,5 @@
 import {action, computed, observable} from "mobx";
+import {rebase} from "../helpers/firebase";
 
 export class UserStore {
 
@@ -12,6 +13,9 @@ export class UserStore {
   @observable
   displayName = null;
 
+  @observable
+  data = {};
+
   // computeds
   @computed
   get isAuthenticated() {
@@ -20,10 +24,21 @@ export class UserStore {
 
   // actions
   @action
-  authenticate(user) {
-    this.id = user.uid;
-    this.email = user.email;
-    this.displayName = user.displayName;
+  async authenticate(user) {
+    try {
+      const data = await rebase.get('users', {
+        context: this,
+        query: (ref) => ref.where('user_id', '==', user.uid),
+      });
+
+      this.id = user.uid;
+      this.email = user.email;
+      this.displayName = user.displayName;
+      this.data = data[0];
+
+    } catch(error) {
+      console.log('authentication database failed', error);
+    }
   }
 
   @action
